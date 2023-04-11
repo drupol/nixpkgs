d04951920cdc64e211b96db465b7d0cf0b78f11b
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-11 19:38:42 +0200

    phpactor: use `buildPhpProject` builder

--- pkgs/development/tools/phpactor/default.nix
+++ pkgs/development/tools/phpactor/default.nix
@@ -1,73 +1,17 @@
-{ lib, stdenvNoCC, fetchFromGitHub, php, phpPackages }:
+{ lib, fetchFromGitHub, php }:
 
-let
-  version = "2023.01.21";
+php.buildPhpProject (finalAttrs: {
+  pname = "phpactor";
+  version = "2023.04.10";
 
   src = fetchFromGitHub {
     owner = "phpactor";
     repo = "phpactor";
-    rev = version;
-    hash = "sha256-jWZgBEaffjQ5wCStSEe+eIi7BJt6XAQFEjmq5wvW5V8=";
+    rev = finalAttrs.version;
+    hash = "sha256-nEerwOrXsghdLxG1iVK5pNgLkYFJWqmfL4HMRkjiYdI=";
   };
 
-  vendor = stdenvNoCC.mkDerivation rec {
-    pname = "phpactor-vendor";
-    inherit src version;
-
-
-    # See https://github.com/NixOS/nix/issues/6660
-    dontPatchShebangs = true;
-
-    nativeBuildInputs = [
-      php
-      phpPackages.composer
-    ];
-
-    buildPhase = ''
-      runHook preBuild
-
-      substituteInPlace composer.json \
-        --replace '"config": {' '"config": { "autoloader-suffix": "Phpactor",' \
-        --replace '"name": "phpactor/phpactor",' '"name": "phpactor/phpactor", "version": "${version}",'
-      composer install --no-interaction --optimize-autoloader --no-dev --no-scripts
-
-      runHook postBuild
-    '';
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out
-      cp -ar ./vendor $out/
-
-      runHook postInstall
-    '';
-
-    outputHashMode = "recursive";
-    outputHashAlgo = "sha256";
-    outputHash = "sha256-7R6nadWFv7A5Hv14D9egsTD/zcKK5uK9LQlHmwtbKdE=";
-  };
-in
-stdenvNoCC.mkDerivation {
-  pname = "phpactor";
-  inherit src version;
-
-  buildInputs = [
-    php
-  ];
-
-  dontBuild = true;
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/share/php/phpactor $out/bin
-    cp -r . $out/share/php/phpactor
-    cp -r ${vendor}/vendor $out/share/php/phpactor
-    ln -s $out/share/php/phpactor/bin/phpactor $out/bin/phpactor
-
-    runHook postInstall
-  '';
+  vendorHash = "sha256-dR+Y3rf47nwJQ1+bYBO+ap/w/0755/qHwTZWBVkMzb4=";
 
   meta = {
     description = "Mainly a PHP Language Server";
@@ -75,5 +19,4 @@ stdenvNoCC.mkDerivation {
     license = lib.licenses.mit;
     maintainers = lib.teams.php.members ++ [ lib.maintainers.ryantm ];
   };
-
-}
+})
