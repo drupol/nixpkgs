7126eb7c2d27d4e04092acac66dae1cc66a3e076
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-15 04:20:19 +0200

    robo: init at 3.0.4

---
+++ pkgs/development/tools/robo/default.nix
@@ -0,0 +1,22 @@
+{ lib, fetchFromGitHub, php80 }:
+
+php80.buildPhpProject (finalAttrs: {
+  pname = "robo";
+  version = "3.0.4";
+
+  src = fetchFromGitHub {
+    owner = "consolidation";
+    repo = "robo";
+    rev = finalAttrs.version;
+    hash = "sha256-d6IxE1pHfNAIaW2jRwl/7bweNFaovV0cTQSF2CwvD1Q=";
+  };
+
+  vendorHash = "sha256-iCo2MpkhEsZQuOvsgzfmuP7u+DZu2qEykyZzzWoznow=";
+
+  meta = with lib; {
+    description = "Modern task runner for PHP";
+    license = licenses.mit;
+    homepage = "https://github.com/consolidation/robo";
+    maintainers = teams.php.members;
+  };
+})
