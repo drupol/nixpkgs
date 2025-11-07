4c0b2cd7aa6ad29b89eba1ade6c5c9c7b3933da6
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-11-07 09:34:52 +0100

    davis: update `vendorHash`

--- pkgs/by-name/da/davis/package.nix
+++ pkgs/by-name/da/davis/package.nix
@@ -16,9 +16,8 @@ php.buildComposerProject2 (finalAttrs: {
     hash = "sha256-Ih06CKwgR2ljw3w9YfgVUdBCjt5Nbs34fMsErRUkfcc=";
   };
 
-  vendorHash = "sha256-e0qSI5naqM/mUSMduiku0yQkYMGw1y9Uwa5oYlxaDzs=";
-
   composerNoPlugins = false;
+  vendorHash = "sha256-ovkTwQuZgL151pqGanJ20Rm+VqZ+hzLzmNqLaBQNJb8=";
 
   postInstall = ''
     chmod -R u+w $out/share
